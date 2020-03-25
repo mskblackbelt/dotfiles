06f7a3a237ca16eed39e6c0ef424c785fe1c852b
Dustin Wheeler <3818510+mskblackbelt@users.noreply.github.com>
2020-03-25 14:43:09 +0000

    Added UTF-8 support to prompt as default for altchars.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -47,17 +47,28 @@ function setprompt {
   ###
   # See if we can use extended characters to look nicer.
   
-  typeset -A altchar
-  set -A altchar ${(s..)terminfo[acsc]}
-  PR_SET_CHARSET="%{$terminfo[enacs]%}"
-  PR_SHIFT_IN="%{$terminfo[smacs]%}"
-  PR_SHIFT_OUT="%{$terminfo[rmacs]%}"
-  PR_HBAR=${altchar[q]:--}
-  PR_ULCORNER=${altchar[l]:--}
-  PR_LLCORNER=${altchar[m]:--}
-  PR_LRCORNER=${altchar[j]:--}
-  PR_URCORNER=${altchar[k]:--}
-  
+  if [[ $(locale charmap) == "UTF-8" ]]; then
+    PR_SET_CHARSET=""
+    PR_SHIFT_IN=""
+    PR_SHIFT_OUT=""
+    PR_HBAR="─"
+    PR_ULCORNER="┌"
+    PR_LLCORNER="└"
+    PR_LRCORNER="┘"
+    PR_URCORNER="┐"
+  else
+    typeset -A altchar
+    set -A altchar ${(s..)terminfo[acsc]}
+    PR_SET_CHARSET="%{$terminfo[enacs]%}"
+    PR_SHIFT_IN="%{$terminfo[smacs]%}"
+    PR_SHIFT_OUT="%{$terminfo[rmacs]%}"
+    PR_HBAR=${altchar[q]:--}
+    PR_ULCORNER=${altchar[l]:-|}
+    PR_LLCORNER=${altchar[m]:-|}
+    PR_LRCORNER=${altchar[j]:-|}
+    PR_URCORNER=${altchar[k]:-|}
+  fi
+
   ###
   # See if we can use colors.
 
