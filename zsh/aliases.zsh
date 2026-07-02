5e1ff586b6a440dbdce2d9a979667df708f15500
Dustin Wheeler <3818510+mskblackbelt@users.noreply.github.com>
2026-07-02 12:44:44 -0400

    Commend out bat help aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -47,13 +47,13 @@ if _command_exists batcat; then
 	alias bat="batcat"
 	alias cat="bat --terminal-width=-2"
 	# alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-	alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+	# alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 fi
 
 if _command_exists bat; then 
 	alias cat="bat --terminal-width=-2"
-	alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-	alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+	# alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+	# alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 fi
 if _command_exists dust; then 
 	alias du="dust"
